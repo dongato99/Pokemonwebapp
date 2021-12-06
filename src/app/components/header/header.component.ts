f03585d5bb8d0cf5db0844ac0217709582181c21
Pedro Euan <pd.ani99@hotmail.com>
2021-12-06 17:00:32 -0600

    Documentacion headercomponent

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
+/**
+ * Componente Header/navbar
+ */
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -14,11 +17,16 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+/**
+ * Redirección a home
+ */
   home(){
     this.router.navigateByUrl('/home');
   }
-
+/**
+ * Cerrar sesión
+ * @returns 
+ */
   logout(){
     return this.authService.logout()
       .subscribe(
